be7e7c5b971e4a36c5d65fd28d1366b00803c090
Taras Rodynenko <t.rodynenko@gmail.com>
2017-03-22 00:38:51 +0200

    return nginx

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3000);
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
